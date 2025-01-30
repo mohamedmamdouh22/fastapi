c3563c3cff154eb8c62ee1f26c1025704e76b3ce
mohamedmamdouh22 <muhammed195896@feng.bu.edu.eg>
2025-01-30 03:13:17 +0200

    Refactor code and update user endpoint to allow for optional query parameter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from enum import Enum
-from typing import Annotated
-from pydantic import BaseModel
+from typing import Annotated, Literal
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -18,7 +18,9 @@ class Gender(str, Enum):
 
 
 @app.get("/user/{user_id}")
-async def get_gender(user_id: int, gender_id: Gender | None = None):
+async def get_gender(
+    user_id: Annotated[int, Path(..., ge=0)], gender_id: Annotated[Gender, Query(...)]
+):
     if gender_id:
         return {"user_id": user_id, "gender": gender_id.value}
     return {"user_id": user_id}
@@ -46,14 +48,26 @@ async def update_gender(user_id: int, user: User, limit: int | None = None):
 # create an end point to update the user data
 
 
+class FilterParams(BaseModel):
+    model_config = {"extra": "forbid"}
+    start: int = Field(0, lt=100, ge=0)
+    limit: int = Field(10, lt=100, ge=0)
+    order_by: Literal["created_at", "updated_at"] = Field(
+        default="created_at", title="Order by"
+    )
+    tags: list[str] = []
+
+
 @app.put("/user/{user_id}")
 async def update_user(
-    user_id: int,
+    *,
+    user_id: Annotated[int, Path(..., gt=0, lt=100, title="User ID")],
     user: User,
-    q: Annotated[list[str] | None, Query(alias="query")] = None
+    q: Annotated[FilterParams, Query(title="Query")],
 ):
     user_dict = user.model_dump()
     if q:
-        user_dict.update({"q": q})
+        user_dict.update(**q.model_dump())
     user_data = {"user_id": user_id, **user_dict}
+
     return user_data
